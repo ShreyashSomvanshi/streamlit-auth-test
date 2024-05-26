ca4347ee9952857b7e72c35b2f803bbec7723d6c
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2024-05-26 23:08:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,55 +28,55 @@ if LOGGED_IN == True:
     st.write('Welcome ',username)
 
 # GitHub URL of your model file (replace with your actual URL)
-github_model_url = "https://raw.githubusercontent.com/ShreyashSomvanshi/test_synthcheck/main/firstModel.h5"
-
-
-# Download the model file
-@st.cache_resource
-def load_model(github_model_url):
+    github_model_url = "https://raw.githubusercontent.com/ShreyashSomvanshi/test_synthcheck/main/firstModel.h5"
+    
+    
+    # Download the model file
+    @st.cache_resource
+    def load_model(github_model_url):
+            
+        response = requests.get(github_model_url)
+        with open("firstModel.h5", "wb") as f:
+            f.write(response.content)
         
-    response = requests.get(github_model_url)
-    with open("firstModel.h5", "wb") as f:
-        f.write(response.content)
+        # Load the model using TensorFlow
+        
+        model = tf.keras.models.load_model("firstModel.h5")
+        return model
     
-    # Load the model using TensorFlow
+    model = load_model(github_model_url)
     
-    model = tf.keras.models.load_model("firstModel.h5")
-    return model
-
-model = load_model(github_model_url)
-
-def classify_image(file_path):
-    # model = load_model('firstModel.h5')
-    image = Image.open(file_path) # reading the image
-    image = image.resize((32, 32)) # resizing the image to fit the trained model   
-    img = np.asarray(image) # converting it to numpy array
-    img = np.expand_dims(img/255, 0)
-    predictions = model.predict(img) # predicting the label
-    if predictions > 0.5:
-        res = 'REAL'
-    else:
-        res = 'SYNTHETIC'
-    return res
-
+    def classify_image(file_path):
+        # model = load_model('firstModel.h5')
+        image = Image.open(file_path) # reading the image
+        image = image.resize((32, 32)) # resizing the image to fit the trained model   
+        img = np.asarray(image) # converting it to numpy array
+        img = np.expand_dims(img/255, 0)
+        predictions = model.predict(img) # predicting the label
+        if predictions > 0.5:
+            res = 'REAL'
+        else:
+            res = 'SYNTHETIC'
+        return res
     
+        
+        
+    st.write("Upload an image to check whether it is a fake or real image.")
     
-st.write("Upload an image to check whether it is a fake or real image.")
-
-file_uploaded = st.file_uploader("Choose the Image File", type=["jpg", "jpeg"])
-
-#Notice: By accessing our website, users acknowledge and accept full responsibility for their activities conducted within the platform. We prioritize your privacy and do not retain or distribute any images you upload
-
-if st.button('Check', use_container_width=True):
-    if file_uploaded is not None:
-        st.toast("Please note that results may not always be accurate. We prioritize your privacy and do not store or distribute any images you upload.",icon="⚠️")
-        res = classify_image(file_uploaded)
-        c1, buff, c2 = st.columns([2, 0.5, 2])
-        c1.image(file_uploaded, use_column_width=True)
-        c2.subheader("Classification Result")
-        c2.write("The image is classified as **{}**.".format(res.title()))
-    else:
-        st.error('Please upload an image to verify.', icon="❌")
+    file_uploaded = st.file_uploader("Choose the Image File", type=["jpg", "jpeg"])
+    
+    #Notice: By accessing our website, users acknowledge and accept full responsibility for their activities conducted within the platform. We prioritize your privacy and do not retain or distribute any images you upload
+    
+    if st.button('Check', use_container_width=True):
+        if file_uploaded is not None:
+            st.toast("Please note that results may not always be accurate. We prioritize your privacy and do not store or distribute any images you upload.",icon="⚠️")
+            res = classify_image(file_uploaded)
+            c1, buff, c2 = st.columns([2, 0.5, 2])
+            c1.image(file_uploaded, use_column_width=True)
+            c2.subheader("Classification Result")
+            c2.write("The image is classified as **{}**.".format(res.title()))
+        else:
+            st.error('Please upload an image to verify.', icon="❌")
 
 
 
